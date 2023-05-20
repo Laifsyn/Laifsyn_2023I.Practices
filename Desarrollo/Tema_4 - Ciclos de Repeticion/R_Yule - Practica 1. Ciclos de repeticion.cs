a99923935ca5e8938fa25857f1951c7772c06df5
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-05-19 20:43:01 -0500

    Start of the Repository.
    printf("Hellow World!");

---
+++ Desarrollo/Tema_4 - Ciclos de Repeticion/R_Yule - Practica 1. Ciclos de repeticion.cs
@@ -0,0 +1,277 @@
+/* Escriba un algoritmo que lea 25 números enteros cualquiera e imprima la siguiente estadística:
+Total de ceros leídos
+Suma de los números negativos leídos
+Promedio de los números de los positivos leídos */
+
+
+Algoritmo NumerosEnteros
+{
+    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
+    inicializar en flotante a sumaPos=0, totPos=0;
+    mientras (cont<25)
+        {
+            escribir ("Entre un numero: ");
+                leer (num);
+            si (num==0)
+                totCeros=totCeros+1;
+            sino
+                si(num<0)
+                    sumaNeg=sumaNeg+num;
+                sino 
+                    {
+                       contPos = contPos + 1;
+                       sumaPos = sumaPos + num; 
+                    }
+                cont = cont + 1;
+        }
+    escribir ("Total de Ceros= ", totCeros);
+    escribir ("Suma de los negativos= ", sumaNeg);
+    si (contPos!=0)
+        escribir ("El promedio de los positivos= ", sumaPos/contPos);
+    sino
+        escribir ("No hay numeros positivos");
+}
+
+
+/* Ciclo Hacer-mientras */
+Algoritmo NumerosEnteros
+{
+    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
+    inicializar en flotante a sumaPos=0, totPos=0;
+    hacer 
+        {
+            escribir ("Entre un numero: ");
+                leer (num);
+            si (num==0)
+                totCeros=totCeros+1;
+            sino
+                si(num<0)
+                    sumaNeg=sumaNeg+num;
+                sino 
+                    {
+                       contPos = contPos + 1;
+                       sumaPos = sumaPos + 1; 
+                    }
+                cont = cont + 1;
+        }
+     mientras (cont<25)
+    escribir ("Total de Ceros= ", totCeros);
+    escribir ("Suma de los negativos= ", sumaNeg);
+    si (contPos!=0)
+        escribir ("El promedio de los positivos= ", sumaPos/contPos);
+    sino
+        escribir ("No hay numeros positivos");
+}
+
+
+/* Ciclo Para */
+Algoritmo NumerosEnteros
+{
+    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
+    inicializar en flotante a sumaPos=0, totPos=0;
+    para (cont=0; cont<25; cont = cont + 1)
+        {
+            escribir ("Entre un numero: ");
+                leer (num);
+            si (num==0)
+                totCeros=totCeros+1;
+            sino
+                si(num<0)
+                    sumaNeg=sumaNeg+num;
+                sino 
+                    {
+                       contPos = contPos + 1;
+                       sumaPos = sumaPos + 1; 
+                    }
+        }
+    escribir ("Total de Ceros= ", totCeros);
+    escribir ("Suma de los negativos= ", sumaNeg);
+    si (contPos!=0)
+        escribir ("El promedio de los positivos= ", sumaPos/contPos);
+    sino
+        escribir ("No hay numeros positivos");
+}
+
+
+
+/* 2. Escriba un algoritmo que lea n cantidad de números enteros cualquiera e imprima la siguiente estadística:
+Total de números leídos que son menores a 25.
+Total de números leídos que se encuentran el rango de 25 y 75, inclusive.
+Total de números leídos que son mayores a 75. */
+
+Algoritmo EnterosRango
+{
+    inicializar en entero a cont = 0, numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+
+    imprimir ("Ingrese la cantidad de numeros que desea evaluar");
+        leer (cantidad);
+    mientras (cont < cantidad)
+        {
+            imprimir ("Ingrese el numero a evaluar ");
+                leer(numero);
+                {
+                    si (numero < 25) entonces
+                        menor25 = menor25 + 1;
+                     sino si ((numero <= 75)) entonces
+                        total_rango = total_rango + 1; 
+                    sino
+                        mayor75 = mayor75 + 1;
+                }
+            cont = cont + 1;
+        }
+    imprimir ("El total de numeros menores a 25 son = ", menor25);
+    imprimir ("El total de numeros que se encuentran entre el rango de 25 a 75 son = ", total_rango);
+    imprimir ("El total de numeros mayores a 75 son = ", mayor75);
+
+}
+
+/* Hacer mientras */
+Algoritmo EnterosRango
+{
+    inicializar en entero a numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+
+    imprimir ("Ingrese la cantidad de numeros que desea evaluar");
+        leer (cantidad);
+    hacer
+        {
+            imprimir ("Ingrese el numero a evaluar ");
+                leer(numero);
+                {
+                    si (numero < 25) entonces
+                        menor25 = menor25 + 1;
+                     sino si ((numero <= 75)) entonces
+                        total_rango = total_rango + 1; 
+                    sino
+                        mayor75 = mayor75 + 1;
+                }
+            cont = cont + 1;
+        }
+        mientras (cont < cantidad);    
+            imprimir ("El total de numeros menores a 25 son = ", menor25);
+            imprimir ("El total de numeros que se encuentran entre el rango de 25 a 75 son = ", total_rango);
+            imprimir ("El total de numeros mayores a 75 son = ", mayor75);
+        
+}
+
+/* Para*/
+Algoritmo EnterosRango
+{
+    inicializar en entero a cont = 0, numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+
+    imprimir ("Ingrese la cantidad de numeros que desea evaluar");
+        leer (cantidad);
+    para (cont = 0; cont < cantidad; cont = cont + 1) 
+        {
+            imprimir ("Ingrese el numero a evaluar ");
+                leer(numero);
+                {
+                    si (numero < 25) entonces
+                        menor25 = menor25 + 1;
+                     sino si ((numero <= 75)) entonces
+                        total_rango = total_rango + 1; 
+                    sino 
+                        mayor75 = mayor75 + 1;
+                }
+        }
+    imprimir ("El total de numeros menores a 25 son = ", menor25);
+    imprimir ("El total de numeros que se encuentran entre el rango de 25 a 75 son = ", total_rango);
+    imprimir ("El total de numeros mayores a 75 son = ", mayor75);
+
+}
+
+/* 3. Escriba un algoritmo que imprima la tabla del 7. Donde el multiplicador debe ir de 1 hasta 10. 
+Ejemplo de la salida.
+7 *  1 =   7
+7 *  2 = 14
+  .     .      .
+7 * 10 = 70 */
+
+Algoritmo MientrasTablaDel7
+{
+    inicializar en entero a multiplicador = 0, multiplicando = 0;
+
+    multiplicador = 1;
+    mientras (multiplicador < 10)
+        {
+            imprimir ("La tabla del número 7= ");
+            imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
+            multiplicador = multiplicador + 1;
+        }
+}
+
+/* HacerMientras */
+Algoritmo HacerMientrasTablaDel7
+{
+    inicializar en entero a multiplicador = 0, multiplicando = 0;
+
+    multiplicador = 1;
+    hacer
+        {
+            imprimir ("La tabla del número 7= ");
+            imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
+            multiplicador = multiplicador + 1;
+        }
+    mientras (multiplicador < 10);
+}
+
+/* Para */
+Algoritmo TablaDel7
+{
+    inicializar en entero a multiplicador = 0, multiplicando = 0;
+
+    para (multiplicador = 1; multiplicador <= 10; multiplicador = multiplicador + 1)
+        {
+            imprimir ("La tabla del número 7= ");
+            imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
+        }
+}
+
+
+
+/* 4. Escribir un algoritmo que imprima las tablas de multiplicar de 4, 5, 6, 7 y 8. Donde el multiplicador debe ir de 1 hasta 10. */
+Algoritmo HacerMientrasTablasDel4Al8
+{
+    inicializar en entero a multiplicando=4, multiplicador=1;
+    hacer
+    {
+        escribir ("Tabla Numero: ", multiplicando);
+        Mientras (multiplicador <=10)
+            {
+                escribir (multiplicando, " * ", multiplicador, " = ", multiplicando * multiplicador);
+                multiplicador = multiplicador +1;
+            }
+        multiplicando = multiplicando + 1;
+    }
+    mientras (multiplicando <=8);
+}
+
+
+    
+/* Ciclo Mientras */
+Algoritmo MientrasTablasDel4Al8
+{
+    inicializar en entero a multiplicando=4, multiplicador=1;
+    Mientras (multiplicando<=8)
+    multiplicando = 4; 
+    {
+        escribir ("Tabla Numero: ", multiplicando);
+        Mientras (multiplicador <=10)
+            {
+                escribir (multiplicando, " * ", multiplicador, " = ", multiplicando * multiplicador);
+                multiplicador = multiplicador +1;
+            }
+        multiplicando = multiplicando + 1;
+    }
+}
+
+/* Ciclo Para */
+Algoritmo tablas
+{
+    inicializar en entero a multiplicando=4, multiplicador=1;
+    para (multiplicando=4; multiplicando<=8; multiplicando = multiplicando + 1)
+    {
+        escribir ("Tabla Número: ", multiplicando);
+        para (multiplicador=1; multiplicador <=10; multiplicador = multiplicador +1 );
+            escribir (multiplicando, " * ", multiplicador, " = ", multiplicando * multiplicador);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Laifsyn_2023I.Practices
+ A repository to compare pseudo codes in university
