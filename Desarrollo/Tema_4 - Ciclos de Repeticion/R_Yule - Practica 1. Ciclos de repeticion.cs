28d0a71793c5060d1d7d2f54495e0466cdf8d4d5
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-05-19 21:16:36 -0500

    Update R_Yule - Practica 1. Ciclos de repeticion.cs
    - Inicialización Imcompleta.
    - Paréntesis Extras.
    - Falta de reinicios de valores.
    - Operadores Lógicos mal utilizados.

--- Desarrollo/Tema_4 - Ciclos de Repeticion/R_Yule - Practica 1. Ciclos de repeticion.cs
+++ Desarrollo/Tema_4 - Ciclos de Repeticion/R_Yule - Practica 1. Ciclos de repeticion.cs
@@ -6,8 +6,8 @@ Promedio de los números de los positivos leídos */
 
 Algoritmo NumerosEnteros
 {
-    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
-    inicializar en flotante a sumaPos=0, totPos=0;
+    inicializar en entero a cont=0, totCeros=0, contPos=0;
+    inicializar en flotante a sumaPos=0, sumaNeg=0, num=0;
     mientras (cont<25)
         {
             escribir ("Entre un numero: ");
@@ -20,7 +20,7 @@ Algoritmo NumerosEnteros
                 sino 
                     {
                        contPos = contPos + 1;
-                       sumaPos = sumaPos + num; 
+                       sumaPos = sumaPos + num; //Estas es calculando el promedio
                     }
                 cont = cont + 1;
         }
@@ -36,21 +36,21 @@ Algoritmo NumerosEnteros
 /* Ciclo Hacer-mientras */
 Algoritmo NumerosEnteros
 {
-    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
-    inicializar en flotante a sumaPos=0, totPos=0;
+    inicializar en entero a cont=0, totCeros=0, contPos=0;
+    inicializar en flotante a sumaPos=0, sumaNeg=0, num=0;
     hacer 
         {
             escribir ("Entre un numero: ");
-                leer (num);
+            leer (num);
             si (num==0)
-                totCeros=totCeros+1;
+                totCeros = totCeros + 1; //Estética
             sino
                 si(num<0)
-                    sumaNeg=sumaNeg+num;
+                    sumaNeg = sumaNeg + num; //Estética
                 sino 
                     {
                        contPos = contPos + 1;
-                       sumaPos = sumaPos + 1; 
+                       sumaPos = sumaPos + num; //Estas es calculando el promedio
                     }
                 cont = cont + 1;
         }
@@ -67,8 +67,8 @@ Algoritmo NumerosEnteros
 /* Ciclo Para */
 Algoritmo NumerosEnteros
 {
-    inicializar en entero a cont=0, totCeros=0, sumaNeg=0, num=0;
-    inicializar en flotante a sumaPos=0, totPos=0;
+    inicializar en entero a cont=0, totCeros=0, contPos=0;
+    inicializar en flotante a sumaPos=0, sumaNeg=0, num=0;
     para (cont=0; cont<25; cont = cont + 1)
         {
             escribir ("Entre un numero: ");
@@ -81,7 +81,7 @@ Algoritmo NumerosEnteros
                 sino 
                     {
                        contPos = contPos + 1;
-                       sumaPos = sumaPos + 1; 
+                       sumaPos = sumaPos + num; //Estas es calculando el promedio
                     }
         }
     escribir ("Total de Ceros= ", totCeros);
@@ -101,8 +101,10 @@ Total de números leídos que son mayores a 75. */
 
 Algoritmo EnterosRango
 {
-    inicializar en entero a cont = 0, numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+    inicializar en entero a cont=0, numero, cantidad, menor25 = 0, mayor75 = 0, total_rango = 0;
 
+    // unset numero porque no es necesario inicializar en 0.
+    // unset cantidad tiene la misma historia.
     imprimir ("Ingrese la cantidad de numeros que desea evaluar");
         leer (cantidad);
     mientras (cont < cantidad)
@@ -112,7 +114,7 @@ Algoritmo EnterosRango
                 {
                     si (numero < 25) entonces
                         menor25 = menor25 + 1;
-                     sino si ((numero <= 75)) entonces
+                    sino si (numero <= 75) entonces
                         total_rango = total_rango + 1; 
                     sino
                         mayor75 = mayor75 + 1;
@@ -128,8 +130,10 @@ Algoritmo EnterosRango
 /* Hacer mientras */
 Algoritmo EnterosRango
 {
-    inicializar en entero a numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+    inicializar en entero a cont=0, numero, cantidad, menor25 = 0, mayor75 = 0, total_rango = 0;
 
+    // unset numero porque no es necesario inicializar en 0.
+    // unset cantidad tiene la misma historia.
     imprimir ("Ingrese la cantidad de numeros que desea evaluar");
         leer (cantidad);
     hacer
@@ -139,14 +143,14 @@ Algoritmo EnterosRango
                 {
                     si (numero < 25) entonces
                         menor25 = menor25 + 1;
-                     sino si ((numero <= 75)) entonces
+                    sino si (numero <= 75) entonces
                         total_rango = total_rango + 1; 
                     sino
                         mayor75 = mayor75 + 1;
                 }
             cont = cont + 1;
         }
-        mientras (cont < cantidad);    
+        mientras (cont < cantidad);    // Si entiendes porqué no ``(cont <= cantidad)``?
             imprimir ("El total de numeros menores a 25 son = ", menor25);
             imprimir ("El total de numeros que se encuentran entre el rango de 25 a 75 son = ", total_rango);
             imprimir ("El total de numeros mayores a 75 son = ", mayor75);
@@ -156,8 +160,11 @@ Algoritmo EnterosRango
 /* Para*/
 Algoritmo EnterosRango
 {
-    inicializar en entero a cont = 0, numero = 0, cantidad = 0, menor25 = 0, mayor75 = 0, total_rango = 0;
+    inicializar en entero a cont, numero, cantidad, menor25 = 0, mayor75 = 0, total_rango = 0;
 
+    // unset cont solo para que sepas que no es necesario asignarle valor debido al Para() definido abajo.
+    // unset numero tiene similar historia.
+    // unset cantidad tiene similar historia.
     imprimir ("Ingrese la cantidad de numeros que desea evaluar");
         leer (cantidad);
     para (cont = 0; cont < cantidad; cont = cont + 1) 
@@ -167,7 +174,7 @@ Algoritmo EnterosRango
                 {
                     si (numero < 25) entonces
                         menor25 = menor25 + 1;
-                     sino si ((numero <= 75)) entonces
+                    sino si (numero <= 75) entonces
                         total_rango = total_rango + 1; 
                     sino 
                         mayor75 = mayor75 + 1;
@@ -188,10 +195,10 @@ Ejemplo de la salida.
 
 Algoritmo MientrasTablaDel7
 {
-    inicializar en entero a multiplicador = 0, multiplicando = 0;
+    inicializar en entero a multiplicador = 1;
 
-    multiplicador = 1;
-    mientras (multiplicador < 10)
+
+    mientras (multiplicador <= 10)
         {
             imprimir ("La tabla del número 7= ");
             imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
@@ -202,16 +209,15 @@ Algoritmo MientrasTablaDel7
 /* HacerMientras */
 Algoritmo HacerMientrasTablaDel7
 {
-    inicializar en entero a multiplicador = 0, multiplicando = 0;
+    inicializar en entero a multiplicador = 1;
 
-    multiplicador = 1;
     hacer
         {
             imprimir ("La tabla del número 7= ");
             imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
             multiplicador = multiplicador + 1;
         }
-    mientras (multiplicador < 10);
+    mientras (multiplicador <= 10);
 }
 
 /* Para */
@@ -219,7 +225,7 @@ Algoritmo TablaDel7
 {
     inicializar en entero a multiplicador = 0, multiplicando = 0;
 
-    para (multiplicador = 1; multiplicador <= 10; multiplicador = multiplicador + 1)
+    para (multiplicador = 1; multiplicador <= 10; multiplicando++) // <= Estética. pero En Java > miVar++; < segun el profe
         {
             imprimir ("La tabla del número 7= ");
             imprimir (7, " x ", multiplicador," = ", 7 * multiplicador);
@@ -234,13 +240,14 @@ Algoritmo HacerMientrasTablasDel4Al8
     inicializar en entero a multiplicando=4, multiplicador=1;
     hacer
     {
+        multiplicador = 1;
         escribir ("Tabla Numero: ", multiplicando);
         Mientras (multiplicador <=10)
             {
                 escribir (multiplicando, " * ", multiplicador, " = ", multiplicando * multiplicador);
                 multiplicador = multiplicador +1;
             }
-        multiplicando = multiplicando + 1;
+        multiplicando++; // <= Estética
     }
     mientras (multiplicando <=8);
 }
@@ -254,13 +261,14 @@ Algoritmo MientrasTablasDel4Al8
     Mientras (multiplicando<=8)
     multiplicando = 4; 
     {
+        multiplicador = 1;
         escribir ("Tabla Numero: ", multiplicando);
         Mientras (multiplicador <=10)
             {
                 escribir (multiplicando, " * ", multiplicador, " = ", multiplicando * multiplicador);
                 multiplicador = multiplicador +1;
             }
-        multiplicando = multiplicando + 1;
+        multiplicando++; // <= Estética
     }
 }
 
